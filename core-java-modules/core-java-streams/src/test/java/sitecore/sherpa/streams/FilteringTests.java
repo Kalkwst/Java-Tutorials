a25cae01946aad267bced7644a582e8b2ec82b5c
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-10-31 10:14:00 +0200

    :white_check_mark: Finalize unit tests for Filtering

--- core-java-modules/core-java-streams/src/test/java/sitecore/sherpa/streams/FilteringTests.java
+++ core-java-modules/core-java-streams/src/test/java/sitecore/sherpa/streams/FilteringTests.java
@@ -48,4 +48,107 @@ public class FilteringTests {
 
         assertEquals(expected, result);
     }
+
+    @Test
+    public void filterByFirstLetter_whenProvidedWithAnArray_returnsOnlyValidStrings() {
+        List<String> fruits = Arrays.asList("apple", "banana", "cherry", "grape");
+        String letter = "b";
+
+        List<String> filteredList = Filtering.filterByFirstLetter(fruits, letter);
+
+        assertEquals("banana", filteredList.get(0));
+    }
+
+    @Test
+    public void filterByFirstLetter_whenProvidedWithNoMatchingElements_returnsEmptyArray() {
+        List<String> fruits = Arrays.asList("apple", "banana", "cherry", "grape");
+        String letter = "z";
+
+        List<String> filteredList = Filtering.filterByFirstLetter(fruits, letter);
+
+        assertEquals(filteredList.size(), 0);
+    }
+
+    @Test
+    public void filterByLastLetter_whenProvidedWithAnArray_returnsOnlyValidStrings() {
+        List<String> fruits = Arrays.asList("apple", "banana", "cherry", "grape");
+        String letter = "e";
+
+        List<String> filteredList = Filtering.filterByLastLetter(fruits, letter);
+
+        assertEquals("apple", filteredList.get(0));
+        assertEquals("grape", filteredList.get(1));
+
+    }
+
+    @Test
+    public void filterByLastLetter_whenProvidedWithNoMatchingElements_returnsEmptyArray() {
+        List<String> fruits = Arrays.asList("apple", "banana", "cherry", "grape");
+        String letter = "z";
+
+        List<String> filteredList = Filtering.filterByLastLetter(fruits, letter);
+
+        assertEquals(filteredList.size(), 0);
+    }
+
+    @Test
+    public void filterBySubstring_whenProvidedWithMatchingElements_returnsFilteredResults() {
+        List<String> inputList = Arrays.asList("apple", "banana", "cherry", "grape");
+        String substring = "an";
+
+        List<String> filteredList = Filtering.filterBySubstring(inputList, substring);
+
+        assertEquals("banana", filteredList.get(0));
+    }
+
+    @Test
+    public void filterBySubstring_whenProvidedWithNoMatchingElement_returnsEmptyArray() {
+        List<String> inputList = Arrays.asList("apple", "banana", "cherry", "grape");
+        String substring = "lz";
+
+        List<String> filteredList = Filtering.filterBySubstring(inputList, substring);
+
+        assertEquals(0, filteredList.size());
+    }
+
+    @Test
+    public void filterByAnySubString_whenProvidedWithMatchingElements_returnsFilteredResults() {
+        List<String> inputList = Arrays.asList("apple", "banana", "cherry", "grape");
+        List<String> substrings = Arrays.asList("an", "rry");
+
+        List<String> filteredList = Filtering.filterByAnySubString(inputList, substrings);
+
+        assertEquals("banana", filteredList.get(0));
+        assertEquals("cherry", filteredList.get(1));
+    }
+
+    @Test
+    public void filterByAnySubString_whenProvidedWithNoMatchingElements_returnsFilteredResults() {
+        List<String> inputList = Arrays.asList("apple", "banana", "cherry", "grape");
+        List<String> substrings = Arrays.asList("lz", "bp");
+
+        List<String> filteredList = Filtering.filterByAnySubString(inputList, substrings);
+
+        assertEquals(0, filteredList.size());
+    }
+
+    @Test
+    public void filterByAllSubStrings_whenProvidedWithMatchingElements_returnsFilteredResults() {
+        List<String> inputList = Arrays.asList("apple", "banana", "cherry", "grape");
+        List<String> substrings = Arrays.asList("ba", "na");
+
+        List<String> filteredList = Filtering.filterByAllSubStrings(inputList, substrings);
+
+        assertEquals("banana", filteredList.get(0));
+    }
+
+    @Test
+    public void filterByAllSubString_whenProvidedWithNoMatchingElements_returnsFilteredResults() {
+        List<String> inputList = Arrays.asList("apple", "banana", "cherry", "grape");
+        List<String> substrings = Arrays.asList("lz", "bp");
+
+        List<String> filteredList = Filtering.filterByAllSubStrings(inputList, substrings);
+
+        assertEquals(0, filteredList.size());
+    }
 }
